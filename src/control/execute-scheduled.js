59305001df044c120a4be0b4ecd46af910d1e182
rickzx98 <rickzx98@gmail.com>
2016-12-05 01:01:58 +0800

    updates

--- src/control/execute-scheduled.js
+++ src/control/execute-scheduled.js
@@ -13,41 +13,55 @@ export default class ExecuteScheduled {
             if (errScheduledJob) {
                 callback(errScheduledJob);
             } else {
-                const jobId = jobData._id;
-                const data = scheduledJobResult.data.data;
-                const scheduled = data.scheduled;
-                const nextEventJobId = scheduled.nextEventJobId;
-                const dateTime = scheduled.dateTime;
-                Schedule.scheduleJob(new Date(dateTime), () => {
-                    updateJobStatus.execute({
-                        params: {
-                            eventJobId: nextEventJobId,
-                            status: 'NEW'
-                        }
-                    }, errNextJob => {
-                        if (errNextJob) {
-                            updateJobStatus.execute({
-                                params: {
-                                    eventJobId: jobId,
-                                    status: 'LOCKED'
-                                }
-                            }, () => {
-                                global.gdsLogger.logError(new Error('Scheduled event failed for job id ' + jobId));
-                            });
-                        } else {
-                            updateJobStatus.execute({
-                                params: {
-                                    eventJobId: jobId,
-                                    status: 'COMPLETED'
-                                }
-                            }, () => {
-                                global.gdsLogger.logInfo('Scheduled event has been triggered for job id ' + jobId);
-                            });
-                        }
-                    });
-                });
+                new CheckSchedule(jobData, scheduledJobResult, updateJobStatus);
                 callback();
             }
         });
+    }
+}
+class CheckSchedule {
+    constructor(jobData, scheduledJobResult, updateJobStatus) {
+        const curDate = new Date();
+        curDate.setMilliseconds(0);
+        const jobId = jobData._id;
+        const data = scheduledJobResult.data.data;
+        const scheduled = data.scheduled;
+        const nextEventJobId = scheduled.nextEventJobId;
+        const dateTime = scheduled.dateTime;
+        console.log('time1 =>', curDate.getTime());
+        console.log('time2 =>', new Date(dateTime).getTime());
+        console.log('checking => ', curDate.getTime() === new Date(dateTime).getTime());
+        if (curDate.getTime() === new Date(dateTime).getTime()) {
+            updateJobStatus.execute({
+                params: {
+                    eventJobId: nextEventJobId,
+                    status: 'NEW'
+                }
+            }, errNextJob => {
+                if (errNextJob) {
+                    updateJobStatus.execute({
+                        params: {
+                            eventJobId: jobId,
+                            status: 'LOCKED'
+                        }
+                    }, () => {
+                        global.gdsLogger.logError(new Error('Scheduled event failed for job id ' + jobId));
+                    });
+                } else {
+                    updateJobStatus.execute({
+                        params: {
+                            eventJobId: jobId,
+                            status: 'COMPLETED'
+                        }
+                    }, () => {
+                        global.gdsLogger.logInfo('Scheduled event has been triggered for job id ' + jobId);
+                    });
+                }
+            });
+        } else {
+            setTimeout(function() {
+                new CheckSchedule(jobData, scheduledJobResult, updateJobStatus);
+            }, 1000);
+        }
     }
 }
